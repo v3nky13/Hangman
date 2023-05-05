91568b0e7d4eb01cbccbbd92b9234d8d0ca897d1
v3nky13 <venkateswar1372@gmail.com>
2023-05-05 10:39:35 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,102 @@
+const phrases = [
+  'ping pong',
+  'piano',
+  'earthquake',
+  'skateboard',
+  'alarm clock',
+  'money',
+  'banana peel',
+  'think',
+  'shopping cart',
+  'newspaper',
+  'hula hoop',
+  'rain',
+  'seesaw',
+  'jazz',
+  'hangman',
+  'zebra',
+  'subway',
+  'blizzard',
+  'buffalo',
+  'quantum',
+  'cow',
+  'puppy',
+  'glyph',
+  'transparent',
+  'funny',
+];
+
+let phrase = '';
+let guessed = '';
+let countLeft = 0;
+
+const createGuessed = () => {
+  guessed = '';
+  for (let i = 0; i < phrase.length; i++)
+    guessed += phrase[i] === ' ' ? ' ' : '_';
+};
+
+const prettyOutput = (str) => {
+  let prettyOutput = '';
+  for (let i = 0; i < str.length; i++)
+    if (str[i] !== ' ') prettyOutput += ' ' + str[i] + ' ';
+    else prettyOutput += '&nbsp;&nbsp;';
+  return prettyOutput;
+};
+
+const newPhrase = () => {
+  countLeft = 6;
+  document.getElementById('winning-status').innerHTML = 'Guess:';
+  buttons = document.getElementsByClassName('letter');
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].style.backgroundColor = '#ebecf0';
+    buttons[i].disabled = false;
+    buttons[i].classList.add('letter-hover');
+  }
+  phrase = phrases[Math.floor(Math.random() * phrases.length)];
+  console.log(phrase);
+  createGuessed();
+  document.getElementById('phrase-text').innerHTML = prettyOutput(guessed);
+};
+
+const replaceAt = (str, index, replacement) => {
+  return (
+    str.substring(0, index) +
+    replacement +
+    str.substring(index + replacement.length)
+  );
+};
+
+const updateGuessed = (ch) => {
+  let crtGuess = false;
+  for (let i = 0; i < guessed.length; i++) {
+    if (guessed[i] === '_' && phrase[i] === ch) {
+      guessed = replaceAt(guessed, i, ch);
+      crtGuess = true;
+    }
+  }
+  return crtGuess;
+};
+
+const playerWon = () => {
+  let won = true;
+  for (let i = 0; i < guessed.length; i++) if (guessed[i] == '_') won = false;
+  return won;
+};
+
+const newGuess = (id) => {
+  const button = document.getElementById(id);
+  const crtGuess = updateGuessed(id);
+  button.disabled = true;
+  button.classList.remove('letter-hover');
+  if (!crtGuess) countLeft--;
+  button.style.backgroundColor = crtGuess ? '#50C878' : '#ff6863';
+  const won = playerWon();
+  document.getElementById('phrase-text').innerHTML =
+    won || !countLeft ? prettyOutput(phrase) : prettyOutput(guessed);
+  if (won) document.getElementById('winning-status').innerHTML = 'You Win!';
+  else if (!countLeft)
+    document.getElementById('winning-status').innerHTML = 'You Lost!';
+};
+
+newPhrase();
